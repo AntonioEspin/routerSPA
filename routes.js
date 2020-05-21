bc9b42284441144fe8c69abf20a8008c81dddef8
Antonio Espin <antoesdev91@gmail.com>
2020-05-21 12:43:01 -0500

    first commit sources

---
+++ routes.js
@@ -0,0 +1,18 @@
+const routes = [
+    {
+        path: '/index.html',
+        template: '<h1>Bienvenido a esta página</h1>'
+    },
+    {
+        path: '/',
+        template: `<h1>Estoy en el home</h1>`
+    },
+    {
+        path: '/contacto',
+        template: `<h1>Contactame a:</h1>`
+    },
+    {
+        path: '/aboutme',
+        template: `<h1>Acerca de mi</h1>`
+    },
+];
